8f29120e97c72dda2c714fa5411fca81aa82881d
Pranshu Swami <pranshuswami185@gmail.com>
2026-06-15 11:15:56 +0530

    CURD operations

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express=require("express")
 const mysql=require("mysql2")
-const cors=require("cors")
+
 const app=express()
 app.use(express.json())
-app.use(cors())
+
 const db=mysql.createConnection({
     host:"localhost",
     user:"root",
@@ -34,71 +34,75 @@ app.get("/customer-order",(req,res)=>{
         }
     })
 })
-app.get("/products-category",(req,res)=>{
-    const sql="SELECT p.product_name,p.price,c.category_name FROM products p JOIN categories c ON p.category_id=c.category_id"
-    db.query(sql,(err,result)=>{
-        if(err){
-            return res.status(500).json({
-                status:"failed",
-                message:"data not fetched"
-            })
-        }else{
-            res.status(200).json({
-                status:"success",
-                message:"data fetched",
-                data:result
-            })
-        }
-    })
-})
-app.get("/order-product",(req,res)=>{
-     const sql="SELECT o.order_id,o.order_date,oi.quantity,oi.price,p.product_name,p.product_id FROM orders o JOIN order_items oi ON o.order_id=oi.order_id JOIN products p ON oi.product_id=p.product_id"
-    db.query(sql,(err,result)=>{
-        if(err){
-            return res.status(500).json({
-                status:"failed",
-                message:"data not fetched"
-            })
-        }else{
-            res.status(200).json({
-                status:"success",
-                message:"data fetched",
-                data:result
-            })
-        }
-    })
-})
-app.get("/customer-purchase",(req,res)=>{
-    const sql="SELECT c.name,c.email,o.order_id,o.order_date,oi.quantity,oi.price,p.product_name,p.price FROM customers c JOIN orders o ON c.customer_id=o.customer_id JOIN order_items oi ON o.order_id=oi.order_id JOIN products p ON oi.product_id=p.product_id"
+app.post("/products",(req,res)=>{
+    const{product_name,price,category_id}=req.body
+    const sql="INSERT INTO products(product_name,price,category_id) VALUES(?,?,?)"
 
-    db.query(sql,(err,result)=>{
+    db.query(sql,[product_name,price,category_id],(err,result)=>{
         if(err){
             return res.status(500).json({
                 status:"failed",
-                message:"data not fetched"
+                message:"product not added"
             })
         }else{
             res.status(200).json({
                 status:"success",
-                message:"data fetched",
-                data:result
+                message:"product added successfully",
+            
+            })
+        }
+    })
+}),
+app.put("/products/:id",(req,res)=>{
+    const{id}=req.params
+    const{product_name,price,category_id,}=req.body
+    const sql="UPDATE products SET product_name=?,price=?,category_id=? WHERE product_id=?"
+     db.query(sql,[product_name,price,category_id,id],(err,result)=>{
+        if(err){
+            return res.status(500).json({
+                status:"failed",
+                message:"product not updated"
+            })
+        }else{
+            res.status(200).json({
+                status:"success",
+                message:"product updated successfully"
+            
             })
         }
     })
 })
-app.get("/customer-category",(req,res)=>{
-    const sql="SELECT c.name, c.email,o.order_id,o.order_date,oi.quantity,oi.price,p.product_name,p.price,cg.category_name FROM customers c JOIN orders o ON c.customer_id=o.customer_id JOIN order_items oi ON o.order_id=oi.order_id JOIN products p ON oi.product_id=p.product_id JOIN categories cg ON p.category_id=cg.category_id"
-
-    db.query(sql,(err,result)=>{
+app.delete("/products/:id",(req,res)=>{
+    const{id}=req.params
+    const sql="DELETE FROM products WHERE product_id=?"
+    db.query(sql,[id],(err,result)=>{
         if(err){
             return res.status(500).json({
                 status:"failed",
-                message:"data not fetched"
+                message:"product not deleted"
             })
         }else{
             res.status(200).json({
                 status:"success",
-                message:"data fetched",
+                message:"product deleted successfully"
+            
+            })
+        }
+    })
+})
+app.get("/products/search",(req,res)=>{
+    const{name}=req.query
+    const sql="SELECT * FROM products WHERE product_name LIKE ?"
+    db.query(sql,[`%${name}%`],(err,result)=>{
+        if(err){
+            return res.status(500).json({
+                status:"failed",
+                message:"product not found"
+            })
+        }else{
+            res.status(200).json({
+                status:"success",
+                message:"product founded",
                 data:result
             })
         }
